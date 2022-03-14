36225e1ce7da7cdc1367657ffc7e47603f51138d
Abhijeet Sawant <sawantabhie@gmail.com>
2022-03-14 12:01:34 +0530

    minor layout changes

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,23 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/.vue'
+// import HelloWorld from '@/components/.vue'
+import Cart from '@/components/Cart.vue'
 
-describe('HelloWorld.vue', () => {
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg }
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
+
+describe('Cart.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+    const cartItems = [{ title: 'title', price: 1000 }]
+    const wrapper = shallowMount(Cart, {
+      propsData: { cartItems }
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.contains).toContain(cartItems)
   })
 })
